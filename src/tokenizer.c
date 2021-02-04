53d497afb1e91285468a8bbdc87525dfd67f0d13
Noah P <npizza10@gmail.com>
2021-02-04 02:52:10 -0700

    created tokenizer c file

---
+++ src/tokenizer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+
+  /*
+    First milestone - create a simple UI that takes in user input and shoots out 
+    user input in same order
+  */
+  printf(">\n");
+
+}
