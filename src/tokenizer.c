7a264825c2ce4aec9f85fbd9c437f80e05956b30
Noah P <npizza10@gmail.com>
2021-02-08 16:45:44 -0700

    updated comments on symb const

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,17 +2,34 @@
 
 //symbolic constant
 #define MAX 20
+#define LIMIT 100
+char arr[LIMIT];
 
 void main(){
 
   /*
     First milestone - create a simple UI that takes in user input and shoots out 
     user input in same order
-  */
-
+  */  
   char input[MAX];
   printf("* GREETINGS USER *\n");
   printf("> ");
   scanf("%s",input);
   printf("%s\n", input);
-}
+  
+  /*
+  //OSCARS WAY
+  printf("> ");
+  int i = 0;
+    for(char input_char ; (input_char = getchar()) != '\n' && i < LIMIT;i++){
+    arr[i] = input_char;
+    //putchar(input_char);
+  }
+  printf("%s\n",arr);
+  */
+  
+  /*
+   TODO-  SPACE_CHAR AND NON_SPACE_CHAR
+   */
+ 
+}//main
