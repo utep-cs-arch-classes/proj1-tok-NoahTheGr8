51d835280ca62957821effebc8d99e2f7b321d13
Noah P <npizza10@gmail.com>
2021-02-08 01:40:12 -0700

    finish 1st milestone

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+//symbolic constant
+#define MAX 20
+
 void main(){
 
   /*
     First milestone - create a simple UI that takes in user input and shoots out 
     user input in same order
   */
-  printf(">\n");
 
+  char input[MAX];
+  printf("* GREETINGS USER *\n");
+  printf("> ");
+  scanf("%s",input);
+  printf("%s\n", input);
 }
