da5f64c71c3ab355ab51c9c3e21c7df34f51e3da
R Noah Padilla <rnpadilla2@miners.utep.edu>
2021-02-27 22:19:03 -0700

    Completed demonstrating copy_str() works with tests in its section in the main

--- src/runner.c
+++ src/runner.c
@@ -39,20 +39,24 @@ void main(){
   char *ptr = word_start(usr_input);
   printf("*Actual address of first char: %p\n", &usr_input[3]);//FOR TESTING - change if you are user other inputs
   printf("ptr address of first char: %p\n", ptr);
-  */
+  
   //------------------- WORD END -------------------
   //Assume you start on a non_space char
   char *ptr1 = word_end(usr_input);
   printf("*Actual address of first space char: %p\n", &usr_input[3]);//change if you are using other inputs
   printf("ptr address of first space char: %p\n", ptr1);
-  
+  */
   //------------------- COUNT WORDS -------------------
   int totWords = count_words(usr_input);
   printf("Total Words in input> %d\n", totWords);
 
   //------------------- COPY STR -------------------
-  char *clone = copy_str(usr_input, 2 );
-  printf("USER INPUT addess: %p\n", &usr_input);
-  printf("CLONE address: %p\n", clone);
-  
+  char *clone = copy_str(usr_input, 10);
+  printf("USER INPUT address: %p\n", &usr_input);
+  printf("COPIED address: %p\n", clone);
+
+  printf("USER INPUT: %s\n", usr_input);
+  //printf("USER INPUT size: %lu\n", sizeof(usr_input));
+  printf("COPIED OUTPUT: %s\n", clone);
+  //printf("COPIED OUTPUT size: %d\n", sizeof(clone));
 }//main
