08b3bc25bff98a0adb57c1a698ca02583e07abba
R Noah Padilla <rnpadilla2@miners.utep.edu>
2021-03-08 00:43:16 -0700

    Updated user input array to reset everytime it gets reused and included a template where history would be used

--- src/runner.c
+++ src/runner.c
@@ -10,47 +10,63 @@
 
 void main(){
 
+  //Welcome User
+  printf("GREETINGS USER\n");
+  
+  //<CREATE LINKED LIST HERE>
+  //List *list = init_history();
   char keep_going = 1;//updates if user wants to continue or end program
   while(keep_going){
-    //Welcome User
-    printf("GREETINGS USER\n");
 
     //---------------------- START WHILE  
     //Get Input
     char c;
     int ind = 0; //ind is index for storing chars in usr_input string
     char usr_input[LIMIT]; //Stores an input of size LIMIT
+    printf("---------------------\n");
     printf("IN > ");
     while( (c=getchar()) != '\n' ){
       usr_input[ind] = c;
       ind++;  
     }//while
+    //Add input to linked list here
 
     printf("Input: ");
     printf(usr_input);
     printf("\n");
     
-    printf("------------TOKENIZE------------\n"); 
+    //------------TOKENIZE------------
     if(count_words(usr_input) != 0 ){
       char **tokens = tokenize(usr_input);
       print_tokens(tokens);
       free_tokens(tokens);
-      print_tokens(tokens);//used to see if tokens are no longer there
+      //print_tokens(tokens);//used to see if tokens are no longer there
     }
     else{
       printf("NO VALUES TO TOKENIZE\n");
     }
 
     //ASK USER IF THEY WANT TO DO IT AGAIN
-    char kg;
-    printf("\nTokenizer again? [y,n] > ");
+    char in;
+    printf("\nTokenize again? [(y)es, (n)o, (h)ist] > ");
     while( (c=getchar()) != '\n' ){
-      kg = c; 
+      in = c; 
     }//while
-    
-    if(kg == 'n'){
+
+    //Exit program if use does not want to keep going
+    if(in == 'n'){
       keep_going = 0;
-      //will exit outter most while since user does not want to continue
+    }
+    else{
+      //reset the input before getting new input
+      int i;
+      for(i = 0; i < sizeof(usr_input)/sizeof(usr_input[0]); i++){
+	usr_input[i] = 0;
+      }
+    }//else
+    //Show previous inputs
+    if(in == 'h'){
+      // print_history(list);
     }
     
   }//END OUTTER WHILE
